68f3f75d8c5989a004993a899250bd0cfc894acd
Shohei Akita <fiberalph@gmail.com>
2016-03-02 07:27:23 +0000

    fix

---
+++ manager/includes/lang/state/japanese-utf8_state.inc.php
@@ -0,0 +1,57 @@
+<?php
+/**
+* State List Language File
+* Version: 1.0
+* Date: 02/03/2016
+* Array keys refer to the ID number that is saved in the database
+*
+*/
+
+$_state_lang["1"] = '北海道';
+$_state_lang["2"] = '青森県';
+$_state_lang["3"] = '岩手県';
+$_state_lang["4"] = '宮城県';
+$_state_lang["5"] = '秋田県';
+$_state_lang["6"] = '山形県';
+$_state_lang["7"] = '福島県';
+$_state_lang["8"] = '茨城県';
+$_state_lang["9"] = '栃木県';
+$_state_lang["10"] = '群馬県';
+$_state_lang["11"] = '埼玉県';
+$_state_lang["12"] = '千葉県';
+$_state_lang["13"] = '東京都';
+$_state_lang["14"] = '神奈川県';
+$_state_lang["15"] = '新潟県';
+$_state_lang["16"] = '富山県';
+$_state_lang["17"] = '石川県';
+$_state_lang["18"] = '福井県';
+$_state_lang["19"] = '山梨県';
+$_state_lang["20"] = '長野県';
+$_state_lang["21"] = '岐阜県';
+$_state_lang["22"] = '静岡県';
+$_state_lang["23"] = '愛知県';
+$_state_lang["24"] = '三重県';
+$_state_lang["25"] = '滋賀県';
+$_state_lang["26"] = '京都府';
+$_state_lang["27"] = '大阪府';
+$_state_lang["28"] = '兵庫県';
+$_state_lang["29"] = '奈良県';
+$_state_lang["30"] = '和歌山県';
+$_state_lang["31"] = '鳥取県';
+$_state_lang["32"] = '島根県';
+$_state_lang["33"] = '岡山県';
+$_state_lang["34"] = '広島県';
+$_state_lang["35"] = '山口県';
+$_state_lang["36"] = '徳島県';
+$_state_lang["37"] = '香川県';
+$_state_lang["38"] = '愛媛県';
+$_state_lang["39"] = '高知県';
+$_state_lang["40"] = '福岡県';
+$_state_lang["41"] = '佐賀県';
+$_state_lang["42"] = '長崎県';
+$_state_lang["43"] = '熊本県';
+$_state_lang["44"] = '大分県';
+$_state_lang["45"] = '宮崎県';
+$_state_lang["46"] = '鹿児島県';
+$_state_lang["47"] = '沖縄県';
+?>
